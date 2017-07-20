5a09ff37dcb4d33ef6a6e3008d14919f55bc26b2
Walter <fred@fredcurrie.com>
2017-07-20 15:13:31 -0700

    Fred completed header styles. Check out the animated hamburger icon.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,45 @@
+
+const path = require("path");
+module.exports = {
+    entry: "./app/assets/scripts/app.js",
+    output: {
+        path: path.resolve(__dirname, "./app/temp/scripts"),
+        filename: "App.js"
+    },
+    module: {
+        rules: [ // use to be loaders but is now "rules:"
+                {
+                    loaders: "babel-loader", // need to have -loader after babel
+                    query: {
+                        presets: ["es2015"]
+                    },
+                    test: /\.js$/,
+                    exclude: /node_modules/
+            }
+        ]
+    }
+}
+
+/*const path = require('path');
+
 module.exports = {
     entry: "./app/assets/scripts/App.js",
     output: {
-        path: __dirname + "./app/temp/scripts",
+        path: path.resolve(__dirname, "./app/temp/scripts"),
         filename: "App.js"
+    },
+    
+    module: {
+        loaders: [
+            {
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
     }
-}
+} */
+
